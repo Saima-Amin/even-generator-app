4180e69626cdca405bf988215d26f360d3123539
Saima-Amin <saimaamin4th2022@gmail.com>
2025-05-20 22:19:35 +0600

    code commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+        <h2>Generate Even Numbers</h2>
+        <form action="/generate" method="get">
+            Enter a number: <input type="text" name="n">
+            <input type="submit" value="Generate">
+        </form>
+    '''
+
+@app.route('/generate')
+def generate_even():
+    try:
+        n = int(request.args.get('n', 0))
+        evens = [str(i * 2) for i in range(n)]
+        return f"<h3>First {n} even numbers:</h3><p>{', '.join(evens)}</p>"
+    except ValueError:
+        return "Please enter a valid integer."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
